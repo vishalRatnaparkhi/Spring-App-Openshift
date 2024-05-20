5d7afdc44f29e3f7e9dc4c6f42194eb970e742a4
Vishal Ratnaparkhi <ratnparkhiv@gmail.com>
2024-05-20 23:46:30 +0530

    First commit to Spring App OpenShift

---
+++ src/main/java/com/java/vishal/SpringAppOpenshiftApplication.java
@@ -0,0 +1,24 @@
+package com.java.vishal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAppOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppOpenshiftApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String getMethod()
+	{
+		String name="Hello OpenShift";
+		return  name;
+	}
+
+}
---
+++ src/test/java/com/java/vishal/SpringAppOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.vishal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
